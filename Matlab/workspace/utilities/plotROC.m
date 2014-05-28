f498c1307cea41d9b0b2cfcf0b4cd3e2fbd6cb5a
Marman1337 <marman1@gmail.com>
2014-05-28 15:11:07 +0100

    DRxTEST plotROC

--- Matlab/workspace/utilities/plotROC.m
+++ Matlab/workspace/utilities/plotROC.m
@@ -1,29 +1,29 @@
-function plotROC(white,car,spchspect,babble,opsroom,factory,whitep,carp,spchspectp,babblep,opsroomp,factoryp,snr)
+function plotROC(white,car,spchspect,babble,opsroom,factory,snr)
 
-subplot(3,2,1); plotLine(white,whitep); grid;
+subplot(3,2,1); plotLine(white); grid;
 title(strcat('white',{' '},snr,{' '},'dB')); ylabel('True Positive Rate'); xlabel('False Positive Rate');
-subplot(3,2,2); plotLine(car,carp); grid;
+subplot(3,2,2); plotLine(car); grid;
 title(strcat('car',{' '},snr,{' '},'dB')); ylabel('True Positive Rate'); xlabel('False Positive Rate');
-subplot(3,2,3); plotLine(spchspect,spchspectp); grid;
+subplot(3,2,3); plotLine(spchspect); grid;
 title(strcat('spchspect',{' '},snr,{' '},'dB')); ylabel('True Positive Rate'); xlabel('False Positive Rate');
-subplot(3,2,4); plotLine(babble,babblep); grid;
+subplot(3,2,4); plotLine(babble); grid;
 title(strcat('babble',{' '},snr,{' '},'dB')); ylabel('True Positive Rate'); xlabel('False Positive Rate');
-subplot(3,2,5); plotLine(opsroom,opsroomp); grid;
+subplot(3,2,5); plotLine(opsroom); grid;
 title(strcat('opsroom',{' '},snr,{' '},'dB')); ylabel('True Positive Rate'); xlabel('False Positive Rate');
-subplot(3,2,6); plotLine(factory,factoryp); grid;
+subplot(3,2,6); plotLine(factory); grid;
 title(strcat('factory',{' '},snr,{' '},'dB')); ylabel('True Positive Rate'); xlabel('False Positive Rate');
 
-legend('Sohn','LTSD','Entropy','PARADE','Harmfreq','PEFAC');
-subplot(3,2,1); plotScatter(white,whitep);
-subplot(3,2,2); plotScatter(car,carp);
-subplot(3,2,3); plotScatter(spchspect,spchspectp);
-subplot(3,2,4); plotScatter(babble,babblep);
-subplot(3,2,5); plotScatter(opsroom,opsroomp);
-subplot(3,2,6); plotScatter(factory,factoryp);
+legend('Sohn','LTSD','Entropy','PARADE','Harmfreq');
+subplot(3,2,1); plotScatter(white);
+subplot(3,2,2); plotScatter(car);
+subplot(3,2,3); plotScatter(spchspect);
+subplot(3,2,4); plotScatter(babble);
+subplot(3,2,5); plotScatter(opsroom);
+subplot(3,2,6); plotScatter(factory);
 
 end
 
-function plotLine(results,resultP)
+function plotLine(results)
 
 hold on;
 plot([1;results{1,2}(:,3);0],[1;results{1,2}(:,2);0],'b');
@@ -31,11 +31,10 @@ plot([1;results{2,2}(:,3);0],[1;results{2,2}(:,2);0],'r');
 plot([0;results{3,2}(:,3);1],[0;results{3,2}(:,2);1],'g');
 plot([1;results{4,2}(:,3);0],[1;results{4,2}(:,2);0],'m');
 plot([1;results{5,2}(:,3);0],[1;results{5,2}(:,2);0],'k');
-plot([1;resultP{1,2}(:,3);0],[1;resultP{1,2}(:,2);0],'c');
 
 end
 
-function plotScatter(results,resultP)
+function plotScatter(results)
 
 hold on;
 scatter(results{1,2}(:,3),results{1,2}(:,2),'.b');
@@ -43,6 +42,5 @@ scatter(results{2,2}(:,3),results{2,2}(:,2),'.r');
 scatter(results{3,2}(:,3),results{3,2}(:,2),'.g');
 scatter(results{4,2}(:,3),results{4,2}(:,2),'.m');
 scatter(results{5,2}(:,3),results{5,2}(:,2),'.k');
-scatter(resultP{1,2}(:,3),resultP{1,2}(:,2),'.c');
 
 end
